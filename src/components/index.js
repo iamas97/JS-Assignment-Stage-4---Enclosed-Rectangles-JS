bc28fe5e36240a02f54bfe92003ac8a0022d793f
Aquib <aquibshaikh02397@gmail.com>
2020-11-16 23:53:24 +0530

    changes made a/c to sir

--- src/components/index.js
+++ src/components/index.js
@@ -8,18 +8,20 @@
 
 function updateStructure(rect1,rect2){
 	let rec1 = {...rect1};//JSON.parse(JSON.stringify(rect1));
-	let rec2 = {...rect1};//JSON.parse(JSON.stringify(rect2));
+	let rec2 = {...rect2};//JSON.parse(JSON.stringify(rect2));
 	//write your code
 	const top1     = rec1['top']    !=null ? rec1['top']    : (rec1['bottom'] - rec1['height']);
 	const left1    = rec1['left']   !=null ? rec1['left']   : (rec1['right']  - rec1['width']);
 	const bottom1  = rec1['bottom'] !=null ? rec1['bottom'] : (rec1['height'] + rec1['top']);
 	const right1   = rec1['right']  !=null ? rec1['right']  : (rec1['width']  + rec1['left']);
+
 	const top2     = rec2['top']    !=null ? rec2['top']    : (rec2['bottom'] - rec2['height']);
 	const left2    = rec2['left']   !=null ? rec2['left']   : (rec2['right']  - rec2['width']);
 	const bottom2  = rec2['bottom'] !=null ? rec2['bottom'] : (rec2['height'] + rec2['top']);
 	const right2   = rec2['right']  !=null ? rec2['right']  : (rec2['width']  + rec2['left']);
 	const para = ['top','left','right',"bottom1"]
-	if((top1<=top2 && bottom1>=bottom2 && left1<=left2 && right1>=right2 )||(top1==top2 && bottom1==bottom2 && left1==left2 && right1==right2)){
+	if((top1<=top2 && bottom1<=bottom2 && left1<=left2 && right1<=right2 )||(top1==top2 && bottom1==bottom2 && left1==left2 && right1==right2))
+	{
 		
 		for(let i=0;i<para.length;i++){
 			if(rec2[para[i]]!=null){
@@ -30,7 +32,7 @@ function updateStructure(rect1,rect2){
 		rec1.children.push(rec2);
 		return rec1
 	}
-	if(top1>top2 && bottom1<bottom2 && left1>left2 && right1<right2){
+	if(top1>top2 && bottom1>bottom2 && left1>left2 && right1>right2){
 		
 		for(let i=0;i<para.length;i++){
 			if(rec1[para[i]]!=null){
@@ -38,13 +40,12 @@ function updateStructure(rect1,rect2){
 			}
 		}
 		
-		rec2.children.push(rec2);
+		rec2.children.push(rec1);
 		return rec2
 	}
 	return null;
 }
 
-module.exports = updateStructure;
 
 // let RectangleA=
 // {
@@ -76,3 +77,4 @@ module.exports = updateStructure;
 // 	children:[]
 //    }]
 // }
+module.exports = updateStructure;
