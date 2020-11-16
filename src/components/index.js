c49cef49c92826323406ffcc1d60deca7004904c
Aquib <aquibshaikh02397@gmail.com>
2020-11-17 00:14:52 +0530

    revert back to og code

--- src/components/index.js
+++ src/components/index.js
@@ -14,17 +14,19 @@ function updateStructure(rect1,rect2){
 	const left1    = rec1['left']   !=null ? rec1['left']   : (rec1['right']  - rec1['width']);
 	const bottom1  = rec1['bottom'] !=null ? rec1['bottom'] : (rec1['height'] + rec1['top']);
 	const right1   = rec1['right']  !=null ? rec1['right']  : (rec1['width']  + rec1['left']);
-
+	console.log(top1,left1,bottom1,right1,"Value1");
 	const top2     = rec2['top']    !=null ? rec2['top']    : (rec2['bottom'] - rec2['height']);
 	const left2    = rec2['left']   !=null ? rec2['left']   : (rec2['right']  - rec2['width']);
 	const bottom2  = rec2['bottom'] !=null ? rec2['bottom'] : (rec2['height'] + rec2['top']);
 	const right2   = rec2['right']  !=null ? rec2['right']  : (rec2['width']  + rec2['left']);
-	const para = ['top','left','right',"bottom"]
-	if((top1<=top2 && bottom1<=bottom2 && left1<=left2 && right1<=right2 )||(top1==top2 && bottom1==bottom2 && left1==left2 && right1==right2))
+	console.log(top2,left2,bottom2,right2,"Value2")
+	const para = ['top','left','right',"bottom"];
+	if((top1<=top2 && bottom1>=bottom2 && left1<=left2 && right1>=right2 )||(top1==top2 && bottom1==bottom2 && left1==left2 && right1==right2))
 	{
-		
+		console.log("here in cond 1");
 		for(let i=0;i<para.length;i++){
 			if(rec2[para[i]]!=null){
+				console.log("here in cond 2");
 				rec2[para[i]] = rec2[para[i]] - rec1[para[i]];
 			}
 		}
@@ -43,31 +45,33 @@ function updateStructure(rect1,rect2){
 		rec2.children.push(rec1);
 		return rec2
 	}
+	console.log("here in return");
 	return null;
 }
 
 
-// let RectangleA=
-// {
-// 	top:20, left:20, height:40, width:60,
-// 	position:"absolute",
-// 	children:[]
-// };
-// let RectangleB=
-// {
-// 	top:0, left:20, height:40, width:70,
-// 	position:"relative",
-// 	children:[]
-// };
-// let RectangleB=
-// { 
-// 	top:30, left:30, height:20,width:30,
-// 	position:"absolute",
-// 	children:[]
-// }
-// console.log(updateStructure(RectangleB,RectangleA));
-// console.log("rect b",RectangleB);
+let RectangleA=
+{
+	top:20, left:20, height:40, width:60,
+	position:"absolute",
+	children:[]
+};
+let RectangleB=
+{
+	top:0, left:20, height:40, width:70,
+	position:"relative",
+	children:[]
+};
 
+console.log(updateStructure(RectangleB,RectangleA));
+console.log("rect b",RectangleB);
+
+RectangleB=
+{ 
+	top:30, left:30, height:20,width:30,
+	position:"absolute",
+	children:[]
+}
 // let RectangleA={ 
 // 	top:20, left:20, height:40, width:60,
 // 	position:"absolute",
@@ -78,3 +82,39 @@ function updateStructure(rect1,rect2){
 //    }]
 // }
 module.exports = updateStructure;
+
+RectangleA=
+{
+
+top:20,
+
+left:20,
+
+height:40,
+
+width:60,
+
+position:"absolute",
+
+children:[]
+
+}
+
+
+
+RectangleB=
+{
+
+top:30,
+
+left:30,
+
+height:20,
+
+width:30,
+
+position:"absolute",
+
+children:[]
+
+}
